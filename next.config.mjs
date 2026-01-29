95dda33b4d1d5347b60fdab38e837d1b44649b32
Ahmad Dhavi <163308668+AhmadDhavi@users.noreply.github.com>
2026-01-30 01:32:00 +0700

    Update next.config.mjs with basePath and assetPrefix
    Removed commented-out code and added basePath and assetPrefix.

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,4 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   output: "export",  
-//   images: {
-//     unoptimized: true  
-//   }
-// };
-
-// export default nextConfig;
-
-/** @type {import('next').NextConfig} */
+@type {import('next').NextConfig} 
 const nextConfig = {
   output: "export",
   basePath: "/portfolio",
